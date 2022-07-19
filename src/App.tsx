0af43e8579d7efc2c20e41875ba4bc94b4fa49cc
Henry Tran <huy8691@gmail.com>
2022-07-19 15:06:33 +0700

    core

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { Layout, Menu } from "antd";
-import "antd/dist/antd.css";
-import React, { useState } from "react";
-import Create from "./parts/Create";
-import Update from "./parts/Update";
-import List from "./parts/List";
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+import React, { useState } from 'react';
+import Create from './parts/Create';
+import Update from './parts/Update';
+import List from './parts/List';
 
 const App: React.FC = () => {
-  const { Sider, Content } = Layout;
-  const [data, setData] = useState({});
-  const handleUpdate = (value: any) => {
-    console.log("handle", value);
-    setData(value);
-  };
-  return (
-    <Layout>
-      <Layout>
-        <Sider>
-          <Menu>
-            <Menu.Item>List</Menu.Item>
-          </Menu>
-        </Sider>
-        <Content>
-          <div style={{ margin: "30px" }}>
-            <Create />
-            <Update data={data} />
-          </div>
-          <div style={{ padding: "0px 30px 30px 30px", minHeight: "100vh" }}>
-            <List handleUpdate={handleUpdate} />
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
-  );
+	const { Sider, Content } = Layout;
+	const [data, setData] = useState({});
+	const handleUpdate = (value: any) => {
+		console.log('handle', value);
+		setData(value);
+	};
+	return (
+		<Layout>
+			<Layout>
+				<Sider>
+					<Menu>
+						<Menu.Item>List</Menu.Item>
+					</Menu>
+				</Sider>
+				<Content>
+					<div style={{ margin: '30px' }}>
+						<Create />
+						<Update data={data} />
+					</div>
+					<div style={{ padding: '0px 30px 30px 30px', minHeight: '100vh' }}>
+						<List handleUpdate={handleUpdate} />
+					</div>
+				</Content>
+			</Layout>
+		</Layout>
+	);
 };
 
 export default App;
